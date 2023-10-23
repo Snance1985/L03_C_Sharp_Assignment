24ec9400029979da61649067c756513082b35c04
Sean Nance <sean_nance@hotmail.com>
2023-10-23 10:14:05 -0700

    created c-sharp L03-assignment files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string? readUserInput(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine();
+    }
+
+   string? myName = readUserInput("Enter your name: ");
+        int age;
+
+        string? ageInput = readUserInput("Enter your age: ");
+
+        try
+        {
+            age = int.Parse(ageInput);
+            Console.WriteLine("Name: " + myName);
+            Console.WriteLine("Age: " + age);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid age input. Please enter a valid number.");
+        }
+
+        Console.WriteLine("Your name is " + myName + " and you are " + ageInput + " years old.");
